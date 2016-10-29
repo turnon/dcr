187c2c250c04676955335e47e38ab41a82447579
ken <block24block@gmail.com>
2016-10-29 14:18:47 +0800

    show decorate history

--- lib/dcr.rb
+++ lib/dcr.rb
@@ -1,29 +1,61 @@
 require "dcr/version"
 
 module Dcr
+
+  TRACE = :@_dcr_trace
+
   class << self
+
     def instance klass, org_method_name=nil, &decorator
       org_method_name = parse_method_name(decorator) unless org_method_name
       unbound_org_method = klass.instance_method org_method_name
-      klass.class_eval do
-        define_method unbound_org_method.name do |*args|
-          bound_method = unbound_org_method.bind self
-          decorator.call bound_method, *args
-        end
+
+      new_method = lambda do |*args|
+        bound_method = unbound_org_method.bind self
+        decorator.call bound_method, *args
       end
+
+      klass.class_eval do
+        define_method org_method_name, &new_method
+      end
+
+      set_trace klass, org_method_name, new_method
     end
 
     def singleton object, org_method_name=nil, &decorator
       org_method_name = parse_method_name(decorator) unless org_method_name
       org_method = object.method org_method_name
-      object.define_singleton_method org_method_name do |*args|
-	decorator.call org_method, *args
+
+      new_method = lambda do |*args|
+        decorator.call org_method, *args
       end
+
+      object.define_singleton_method org_method_name, &new_method
+
+      set_trace object, org_method_name, new_method
     end
 
     def parse_method_name a_lambda=nil, &block
       a_lambda = block_given? ? block : a_lambda
       a_lambda.parameters[0][1]
+    end
+
+    def set_trace ko, name, method
+      file, line, _ =  caller[1].split(':')
+      get_trace(ko, name) << [method, file, line.to_i]
+    end
+
+    def get_trace ko, method
+      ko.instance_variable_set(TRACE, {}) unless ko.instance_variable_defined? TRACE
+      trace = ko.instance_variable_get TRACE
+      trace[method] ||= []
+    end
+
+    def list ko, method
+      get_trace(ko, method).
+        map do |method, file, line|
+          [file, line]
+        end
     end
 
   end
